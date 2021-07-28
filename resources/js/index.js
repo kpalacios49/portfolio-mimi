e4d1ed9264eb0f499e6a7324a17a83afca494e9f
Kevin <kevin49palacios@gmail.com>
2021-07-28 00:43:01 -0300

    second version

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,8 +4,8 @@ const myNav = document.getElementById("nav");
 window.onscroll = function() {
     "use strict";
     if (document.body.scrollTop >= 140 || document.documentElement.scrollTop >= 140) {
-      myNav.classList.remove("bg-gradient-1");
+      myNav.classList.add("bg-white");
     } else {
-      myNav.classList.add("bg-gradient-1");
+      myNav.classList.remove("bg-white");
     }
   };
