a4bf67bd67b6e0ca673a3951fa5a025c26f1f12a
Kevin <kevin49palacios@gmail.com>
2021-07-28 20:41:08 -0300

    add responsiveness

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,12 @@
 AOS.init();
 
 const myNav = document.getElementById("nav");
+const menu_btn = document.getElementById("menu-btn");
+const menu = document.querySelector("#menu");
+const menu_btn_close = document.querySelector("#menu-btn-close");
+
+
+
 window.onscroll = function() {
     "use strict";
     if (document.body.scrollTop >= 140 || document.documentElement.scrollTop >= 140) {
@@ -9,3 +15,6 @@ window.onscroll = function() {
       myNav.classList.remove("bg-white");
     }
   };
+
+menu_btn.addEventListener('click', () => menu.style.width == '80%' ? menu.style.width = '0' : menu.style.width = '80%')
+menu_btn_close.addEventListener('click', () => menu.style.width == '80%' ? menu.style.width = '0' : menu.style.width = '80%')
